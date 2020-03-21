632ffecc27d4df836569034f526e31483e404b65
m5loco <45233856+m5loco@users.noreply.github.com>
2020-03-21 15:06:01 -0500

    Optimized

--- DivvyStationAnalyzerApp.R
+++ DivvyStationAnalyzerApp.R
@@ -12,12 +12,12 @@ library(miniUI)
 library(DT)
 library(dplyr)
 
+logo<-"https://github.com/m5loco/Divvy/raw/master/Divvy.jpg"
 
 ##Read Model Results from csv
-x <- getURL("https://raw.githubusercontent.com/m5loco/Divvy/master/new_stations_enhanced.csv")
-ns <- read.csv(text = x)
+ns <- read.csv(text = getURL("https://raw.githubusercontent.com/m5loco/Divvy/master/new_stations_enhanced.csv"))
 
-##Perform some data engineering to prep data for app display and use in leaflet
+##Perform some data engineering on initial load to prep data for app display and use in leaflet
 ns$GEOID<-as.character(ns$GEOID)
 ns$Name<-as.character(ns$Name)
 ns$longitude<-ns$CENTLON
@@ -32,7 +32,7 @@ ns$BG_Tract<-paste(ns$BlkGrp," | ",ns$Tract,sep="")
 ns$age_25_29<-ns$est_female_25_29 + ns$est_male_25_29
 ns$HH_OtherPlus<-ns$HH_Alone+ns$HH_Nonfamily+ns$HH_Other
 
-getTable<-function(selected_mdl,pct)
+getSelectedGeoIds<-function(mdl,pct)
 {
   tree_1<-ns[ns$tree_Yes>pct,'GEOID']
   forest_1<-ns[ns$forest_Yes>pct,'GEOID']
@@ -40,37 +40,36 @@ getTable<-function(selected_mdl,pct)
   xgboost_1<-ns[ns$new_blocks_xgboost>pct,'GEOID']
   all_1<-Reduce(intersect, list(tree_1,forest_1,bayes_1,xgboost_1))
   
-  tree_count<-length(tree_1)
-  forest_count<-length(forest_1)
-  bayes_count<-length(bayes_1)
-  xgboost_count<-length(xgboost_1)
-  all_count<-length(all_1)
-  all_values<-c(tree_count,forest_count,bayes_count,xgboost_count,all_count)
+  if (mdl=="Decision Tree") {geoids<-tree_1}
+  else if (mdl=="Random Forest") {geoids<-forest_1}
+  else if (mdl=="Naive Bayes") {geoids<-bayes_1}
+  else if (mdl=="XGBoost") {geoids<-xgboost_1}
+  else if (mdl=="All Models") {geoids<-all_1}
   
-  mdl_counts<-data.frame(mdl=c("Decision Tree","Random Forest","Naive Bayes","XGBoost","All Models"),
-                         vals=all_values)
-
-  mdl_counts[mdl_counts$mdl==selected_mdl,]
+  geoids
 }
 
-getTable_Blocks<-function(mdl,pct)
+getBlockCount<-function(geoids)
 {
-  tree_1<-ns[ns$tree_Yes>pct,'GEOID']
-  forest_1<-ns[ns$forest_Yes>pct,'GEOID']
-  bayes_1<-ns[ns$bayes_Yes>pct,'GEOID']
-  xgboost_1<-ns[ns$new_blocks_xgboost>pct,'GEOID']
-  all_1<-Reduce(intersect, list(tree_1,forest_1,bayes_1,xgboost_1))
-  
-  if (mdl=="All Models") {geoids<-all_1}
-  if (mdl=="Decision Tree") {geoids<-tree_1}
-  if (mdl=="Random Forest") {geoids<-forest_1}
-  if (mdl=="Naive Bayes") {geoids<-bayes_1}
-  if (mdl=="XGBoost") {geoids<-xgboost_1}
-  
-  nsrows<-ns[ns$GEOID %in% geoids,c("BG_Tract","Median_Family_Income","age_25_29","Transp_Public_betw2024","Transp_Walked",
-                                    "HH_OtherPlus")]
+  length(geoids)
+}
+
+getBlocks<-function(geoids)
+{
+  nsrows<-ns[ns$GEOID %in% geoids,c("BG_Tract","Median_Family_Income","age_25_29",
+                                    "Transp_Public_betw2024","Transp_Walked","HH_OtherPlus")]
   nsrows[order(-nsrows$Median_Family_Income),]
-  
+}
+
+drawMap<-function(lmap,pins)
+{
+  lmap %>%
+    addTiles()%>%
+    addMarkers(
+      lng=pins$longitude,
+      lat=pins$latitude,
+      popup = getPopup(pins),
+      group="myMarkers")
 }
 
 getPopup<-function(pins)
@@ -89,89 +88,57 @@ ui <- miniPage(
   miniTabstripPanel(
     miniTabPanel("Model", icon = icon("sliders"),
                  miniContentPanel(
-                   img(src="https://github.com/m5loco/Divvy/raw/master/Divvy.jpg",height="25"),
+                   img(src=logo,height="25"),
                    titlePanel("Find Location for New Stations:"),
                    radioButtons(inputId = "mdl",
-                               label = "Model:",
-                               choices = list('All Models','Decision Tree','Random Forest','Naive Bayes','XGBoost'),
-                               selected = "All Models"),
+                                label = "Model:",
+                                choices = list('All Models','Decision Tree','Random Forest','Naive Bayes','XGBoost'),
+                                selected = "All Models"),
                    sliderInput(inputId = "pct",
                                label = "Probability of Success:",
                                value=0.75,min=0.50,max=1.00),
-                   div(DT::dataTableOutput("tbl_blk"),style="font-size:75%")
-                   )),
+                   div(DT::dataTableOutput("block_counts"),style="font-size:75%"))),
     miniTabPanel("Census Data", icon = icon("table"),
                  miniContentPanel(
-                   div(DT::dataTableOutput("stbl"),style="font-size:70%")
-                 )),
-   miniTabPanel("Map", icon = icon("map-o"),
-                miniContentPanel(padding = 0,leafletOutput("sMap",height="100%") )
-                )
-  )
+                   div(DT::dataTableOutput("blocks"),style="font-size:70%"))),
+    miniTabPanel("Map", icon = icon("map-o"),
+                 miniContentPanel(padding = 0,leafletOutput("block_map",height="100%")))
+    )
 )
 
 server <- function(input,output){
   
-  output$tbl_blk <- DT::renderDT(getTable(input$mdl,input$pct),
-                             colnames=c("Model","Predicted Successful Blocks"),
-                             options = list(paging=FALSE,searching=FALSE,dom = 't',ordering=FALSE,select=FALSE),rownames=FALSE) 
+  sBlocks <-  reactive({getBlocks(getSelectedGeoIds(input$mdl,input$pct))})
   
-  sSub <-  reactive({
-    getTable_Blocks(input$mdl,input$pct)
+  output$block_counts <- DT::renderDT(data.frame(model=input$mdl,count=length(sBlocks)),
+                                      colnames=c("Model","Predicted Successful Blocks"),
+                                      options = list(paging=FALSE,searching=FALSE,dom = 't',ordering=FALSE,select=FALSE),rownames=FALSE) 
+  
+  output$blocks <- DT::renderDT(datatable(sBlocks(),selection = "multiple",
+                                          colnames=c("BlkGrp | Tract","Med. Income","Age 25-29","Pub Trans btw 20-24","Walker","HH-Other+"),
+                                          rownames=FALSE,
+                                          options=list(paging=TRUE,searching=FALSE,ordering=TRUE,stateSave=TRUE)) 
+                                %>% formatCurrency(c("Median_Family_Income"),"$",digits=0))
+  
+  output$block_map <- renderLeaflet({
+    
+    blocks<-sBlocks()
+    selected_blocks<-blocks[input$blocks_rows_selected,]
+    
+    if (nrow(selected_blocks)>0){pins<-ns[ns[,"BG_Tract"] %in% selected_blocks[,"BG_Tract"],]}
+    else{pins<-ns[ns$BG_Tract %in% blocks$BG_Tract,]}
+    
+    leaflet() %>% drawMap(pins)
+    
   })
   
-  output$stbl <- DT::renderDT(datatable(sSub(),selection = "multiple",
-                                        colnames=c("BlkGrp | Tract","Med. Income","Age 25-29","Pub Trans btw 20-24","Walker","HH-Other+"),
-                                        rownames=FALSE,
-                                        options=list(paging=TRUE,searching=FALSE,ordering=TRUE,stateSave=TRUE)) 
-                              %>% formatCurrency(c("Median_Family_Income"),"$",digits=0))
-  
-  output$sMap <- renderLeaflet({
+  observeEvent(input$blocks_rows_selected, {
     
-    mdl<-input$mdl
-    pct<-input$pct
-    
-    tree_1<-ns[ns$tree_Yes>pct,'GEOID']
-    forest_1<-ns[ns$forest_Yes>pct,'GEOID']
-    bayes_1<-ns[ns$bayes_Yes>pct,'GEOID']
-    xgboost_1<-ns[ns$new_blocks_xgboost>pct,'GEOID']
-    all_1<-Reduce(intersect, list(tree_1,forest_1,bayes_1,xgboost_1))
-    
-    if (mdl=="All Models") {geoids<-all_1}
-    if (mdl=="Decision Tree") {geoids<-tree_1}
-    if (mdl=="Random Forest") {geoids<-forest_1}
-    if (mdl=="Naive Bayes") {geoids<-bayes_1}
-    if (mdl=="XGBoost") {geoids<-xgboost_1}
-    
-    if (length(geoids)>1)
-    {
-      pins<-ns[ns$GEOID %in% geoids,]
-    
-      leaflet() %>%
-      addTiles()%>%
-        addMarkers(lng=pins$longitude,
-                   lat=pins$latitude,
-                   popup = getPopup(pins),
-                   group="myMarkers")
-    }
-  })
-  
-  observeEvent(input$stbl_rows_selected, {
-
-    row_selected = sSub()[input$stbl_rows_selected,]
+    row_selected = sBlocks()[input$blocks_rows_selected,]
     pins<-ns[ns[,"BG_Tract"] %in% row_selected[,"BG_Tract"],]
     
-    proxy <- leafletProxy('sMap')
+    leafletProxy('block_map') %>% clearGroup("myMarkers") %>% drawMap(pins)
     
-    clearGroup(proxy,"myMarkers")
-    
-    proxy %>%
-      addTiles()%>%
-      addMarkers(
-        lng=pins$longitude,
-        lat=pins$latitude,
-        popup = getPopup(pins),
-        group="myMarkers")
   })
   
 }
